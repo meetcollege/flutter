75669389d4f6705e5f0aa99333077476bc53778a
meetcollege <meetn0101@gmail.com>
2025-07-15 10:36:14 +0530

    rowcol

--- mc/lib/home_screen.dart
+++ mc/lib/home_screen.dart
@@ -8,65 +8,78 @@ class HomeScreen extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Center(
-            child: Container(
-              height: 100.0,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                border: const Border.symmetric(vertical: BorderSide()),
+          Row(
+            children: [
+              Center(
+                child: Container(
+                  height: 100.0,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.greenAccent,
+                    border: const Border.symmetric(vertical: BorderSide()),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Center(
+                child: Container(
+                  height: 100.0,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children:[
+            const SizedBox(height: 50),
+            Center(
+              child: Container(
+                height: 100.0,
+                width: 100,
+                color: Colors.amberAccent,
               ),
             ),
-          ),
-          const SizedBox(height: 50),
-          Center(
-            child: Container(
-              height: 100.0,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(100.0),
+            const SizedBox(height: 50),
+            Center(
+              child: Container(
+                height: 100.0,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(100.0),
+                ),
               ),
             ),
+            ],
           ),
-          const SizedBox(height: 50),
-          Center(
-            child: Container(
-              height: 100.0,
-              width: 100,
-              color: Colors.amberAccent,
-            ),
-          ),
-          const SizedBox(height: 50),
-          Center(
-            child: Container(
-              height: 100.0,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(100.0),
+          Row(
+            children :[
+            const SizedBox(height: 50),
+            Center(
+              child: Container(
+                height: 100.0,
+                width: 100,
+                color: Colors.deepOrangeAccent,
               ),
             ),
-          ),
-          const SizedBox(height: 50),
-          Center(
-            child: Container(
-              height: 100.0,
-              width: 100,
-              color: Colors.deepOrangeAccent,
-            ),
-          ),
-          const SizedBox(height: 50),
-          Center(
-            child: Container(
-              height: 100.0,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.lightGreenAccent,
-                borderRadius: BorderRadius.circular(100.0),
+            const SizedBox(height: 50),
+            Center(
+              child: Container(
+                height: 100.0,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.lightGreenAccent,
+                  borderRadius: BorderRadius.circular(100.0),
+                ),
               ),
             ),
+            ],
           ),
+
         ],
       ),
     );
